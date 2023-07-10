a8118bfa5f7aca5224ed238c9750c40c0821f001
utkarshg1 <gaikwadujg@gmail.com>
2023-07-10 19:25:58 +0530

    model trainer completed

--- src/pipelines/training_pipeline.py
+++ src/pipelines/training_pipeline.py
@@ -6,6 +6,7 @@ import pandas as pd
 
 from src.components.data_ingestion import DataIngestion
 from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainer
 
 if __name__=='__main__':
     # Data Ingestion Stage
@@ -17,5 +18,9 @@ if __name__=='__main__':
     data_transformation = DataTransformation()
     train_arr, test_arr, obj_path = data_transformation.initiate_data_transformation(train_data_path,test_data_path)
     print(obj_path)
+
+    # Model Training Stage
+    model_trainer = ModelTrainer()
+    model_trainer.initiate_model_training(train_arr,test_arr)
 
 
